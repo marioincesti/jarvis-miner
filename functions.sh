1080250cd05774e1d42b6312aa26a7922ccb8f18
marioincesti <mario.incesti@vps30.com>
2017-02-19 23:58:17 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -8,3 +8,11 @@
 # You can use translations provided in the language folders functions.sh
 jv_pg_miner () {
 	curl http://zcash.flypool.org/api/miner_new/t1NZzHkYzJx8ZzynPSfvR89hWq3GT1TmNtb | jq -r '.reportedHashRate'
+}
+jv_pg_zecbtc () {
+	curl https://api.coinmarketcap.com/v1/ticker/zcash/ | jq -r '.[]' | jq -r '.price_btc' | cut -c -6
+}
+
+jv_pg_zecusd () {
+	curl https://api.coinmarketcap.com/v1/ticker/zcash/ | jq -r '.[]' | jq -r '.price_usd' | cut -c -4
+}
